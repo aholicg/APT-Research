6b1c33fa6286f32528ec5bf1844806adf6601ecc
Hoàng Trang <168468366+aholicg@users.noreply.github.com>
2026-08-27 15:18:08 +0700

    Add files via upload

---
+++ custom code/Registry Run Key/msgbox.cpp
@@ -0,0 +1,6 @@
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow){
+	MessageBoxA(NULL, "Registry Run key", "Persistence", MB_OK);
+	return 0;
+}
